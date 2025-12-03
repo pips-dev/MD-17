62ab831f19de527073a260e63892d4b01d25ac00
pips-dev <mewissenjf@gmail.com>
2025-12-03 11:41:36 +0100

    premier commit

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from pagerank_fonctions import pageRankLinear, pageRankPower, randomWalk
 def main():
 
     #lecture des fichiers avec la matrice d'adjacence et le vecteur de personnalisation
-    A = np.loadtxt('MatriceAdjacence.csv', delimiter=',')
-    v = np.loadtxt('VecteurPersonnalision.csv', delimiter= ',')
+    A = np.genfromtxt('MatriceAdjacence.csv', delimiter=',', skip_header=1)
+    v = np.genfromtxt('VecteurPersonnalisation_Groupe17.csv', delimiter= ',')
+    print(v.shape)
 
     #paramètre de téléportation
     alpha = 0.9
@@ -13,12 +14,15 @@ def main():
     #appel des fonctions afin de calculer pagerank
     linear = pageRankLinear(A, alpha, v)
     power = pageRankPower(A, alpha, v)
-    random = randomWalk(A, alpha, v)
+    #random = randomWalk(A, alpha, v)
 
     #afficher les résultats obtenus
+    print('this is for linear')
     print(linear)
+    print('\n')
+    print('this is for power')
     print(power)
-    print(random)
+    #print(random)
 
 if (__name__ == '__main__'):
     main()
--- pagerank_fonctions.py
+++ pagerank_fonctions.py
@@ -19,43 +19,40 @@ def pageRankLinear(A: np.matrix, alpha: float, v: np.array) -> np.array:
             else :
                 P[i][j]= 1/n
     I = np.eye(n)
+    print(I)
+    v = v/np.sum(v)
     b = (1-alpha)*v
     x = np.linalg.solve(I-alpha * P, b)
     x = x/np.sum(x)
     return x
 
-
-    
-
 def pageRankPower(A: np.matrix, alpha: float, v: np.array) -> np.array:
     # Implémentation de la power method
     # Affiche les matrices A, P, G, les 3 premières itérations, et le résultat final
-    n = A.shape(0)
-    rows = A.sum(axis=1)
-    P = np.zeros((n,n))
+    n = A.shape[0] #taille de A
+    print(n)
+    rows = A.sum(axis=1) #la somme des lignes de A
+    print(rows)
+    P = np.zeros((n,n)) #matrice n*n remplie de zéros
+    print(P)
     for i in range(n):
-        for j in range(n):
-            if rows[i]== 0:
-                P[i][j] = 1/n
-            P = A/rows[i]
-    I = np.eye(n)
-    b = (1-alpha)*v
-    G =(I - alpha * P) * np.outer(v,np.ones(n))
-    x = np.ones(n)/n
-    while True :
+        if rows[i] > 0:
+            P[i, :] = A[i, :] / rows[i]
+        else:
+            P[i, :] = 1/n
+
+    v = v/np.sum(v)
+    print(v)
+    b = (1-alpha)*np.outer(np.ones(n), v) #application de la formule de la power méthode
+    print(b)
+    G = alpha * P + b
+    print(G)
+    x = np.ones(n)/1/n
+    for m in range (10000):
         new = G @ x
-        if (np.linalg.norm(new - x) < 0.0000000001):
-            return new
+        if (np.linalg.norm(new - x, 1) < 1e-8):
+            return new / np.sum(new)
         x = new
-    return x
-    
-
-
-
-
-
-
-    
 
 
 def randomWalk(A: np.matrix, alpha: float, v: np.array) -> np.array:
