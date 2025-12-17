98e824bf841861135abee686a7ab64e5905f6f74
maxime7077 <maxime7077@outlook.fr>
2025-12-17 15:35:44 +0100

    Graph 2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from pagerank_fonctions import pageRankLinear, pageRankPower, randomWalk
+import matplotlib.pyplot as plt
 
 def main():
 
@@ -14,15 +15,31 @@ def main():
     #appel des fonctions afin de calculer pagerank
     linear = pageRankLinear(A, alpha, v)
     power = pageRankPower(A, alpha, v)
-    #random = randomWalk(A, alpha, v)
+    random, error = randomWalk(A, alpha, v)
 
     #afficher les résultats obtenus
-    print('this is for linear')
+    print('\n This is Linear Method')
     print(linear)
-    print('\n')
-    print('this is for power')
+    print('\n This is Power Method')
     print(power)
-    #print(random)
+    print('\n This is RandomWalk Method')
+    print(random)
+
+    plt.plot(error)
+    plt.title("Graphique des Erreurs par RandomWalk")
+    plt.xlabel("Nombre d'Itérations de la Marche Aléatoire")
+    plt.ylabel("Erreur Moyenne : ϵ")
+    plt.show()
+
+    x = range(10)
+    plt.plot(x, linear, label="Linear")
+    plt.plot(x, power, label="Power")
+    plt.plot(x, random, label="Random Walk")
+    plt.title('Comparaisons des Résolutions de "PageRank"')
+    plt.xlabel("Noeud N")
+    plt.ylabel("Score Pagerank")
+
+    plt.show()
 
 if (__name__ == '__main__'):
     main()
